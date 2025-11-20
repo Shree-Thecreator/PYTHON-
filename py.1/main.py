0c71daec80941f409756ae995a2b8c5b4f34fc21
Shree <shreyaray5829@gmail.com>
2025-11-20 16:25:08 +0530

    PY.1

---
+++ py.1/main.py
@@ -0,0 +1,41 @@
+print("Hello world")
+print(5)
+print(122*88)
+# if you want to break line then you have to use backslash n  - (/n)
+print("hey i am a good girl\nok thats fine doesnt matter")
+print("hey", 6,7, sep="~", end="009\n")
+
+
+# next step
+a = 3
+print(a)
+b = "Shreya"
+print(b)
+c = 9
+print(c)
+print(a+c)
+print("the type of a is" , type(a))
+print("the type of a is" , type(b))
+print("the type of a is" , type(c))
+
+# list
+list1 = [8,7,[4,5],["apple","banana"]]
+print(list1)
+
+ok1 = {"name": "Shakshi", "age": 20, "canVote" : True}
+print(ok1)
+
+
+u = "4"
+h = "9"
+print(int(u)+ int(h))
+
+print(17 / 3 ) # classic division returns a float
+
+
+print(17 // 3)  # floor division discards the fractional part
+
+print (17 % 3 ) # the % operator returns the remainder of the division
+
+print (5 * 3 + 2 ) # floored quotient * divisor + remainder
+# generally in number you dont haave to use cotations .if you use then you have to use "int" in print 
---
+++ py.1/main1.py
@@ -0,0 +1,17 @@
+print (5 ** 2 ) # 5 squared
+
+print (2 ** 7 ) # 2 to the power of 7
+
+width = 20
+height = 5 * 9
+print (width * height)
+
+
+tax = 12.5 / 100
+price = 100.50
+print (price * tax)
+12.5625          # this result is now automatically saved in _
+price = 56.53125
+total = price + price
+rounded_total = round(total, 2)
+print(rounded_total)  # Output: 113.06)

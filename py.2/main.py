b1a0033ced38caad31ffa0b42cba7df316bdb785
Shree <shreyaray5829@gmail.com>
2025-11-20 23:46:33 +0530

    PY.2

---
+++ py.2/main.py
@@ -0,0 +1,12 @@
+# you can't use double quote twice or thrice in a single line. if you have to use that then use \n.
+# or you can use triple single quote or double "" quote to make a line into a string.
+apple = '''Hey hello apple
+"actually i am there dear.'''
+print(apple)
+
+name = "Shreya"
+print(name[0])
+
+print("lets use a for loop\n")
+for character in name:
+    print (character)

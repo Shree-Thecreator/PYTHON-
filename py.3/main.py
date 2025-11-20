a32c2dc5c05f588258fafefb896cdc2beff4f1fb
Shree <shreyaray5829@gmail.com>
2025-11-20 23:47:14 +0530

    PY.3

---
+++ py.3/main.py
@@ -0,0 +1,18 @@
+ok = "Shreya"
+print(ok[-4:-2])
+# -4 means the total number in Shreya is 6 .so 6-4 = 2 .like that 6-2=4 so its going from 2 to 4 ,3rd and 4th means "re"
+
+
+# String methods
+a = "Shreya"
+print(a.upper())
+print(a.lower())
+# upper() is used for block letters and lower is used for small letters
+print(a.replace("Shreya", "Sonai"))
+# like that you can use split,rstrip,count,center,find, startswith, swapcase title etc
+print(a.capitalize()) 
+#  13 python video
+
+
+
+
---
+++ py.3/main2.py
@@ -0,0 +1,14 @@
+# if else statement
+a = int(input("enter your age"))
+print("your age is", a)
+
+
+if(a>18):
+    print("ok you can drive")
+
+# elif(a==0):
+
+
+else:
+    print("no you can not drive")
+    

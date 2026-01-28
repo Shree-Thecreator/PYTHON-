356354964a5119e2023830b8a4184bbebeaac9bc
Shree <shreyaray5829@gmail.com>
2026-01-28 23:07:27 +0530

    Add files via upload

---
+++ py.11/main.py
@@ -0,0 +1,11 @@
+print(list(range(5, 10)))
+
+
+print(list(range(0, 10, 3)))
+
+
+print(list(range(-10, -100, -30)))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
